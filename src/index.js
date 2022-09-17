0b2309eebdd4483759de9c8c948538a002a3893f
Restyled.io <commits@restyled.io>
2022-09-17 09:27:47 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import './index.css';
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App.jsx';
+import App from "./App.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
